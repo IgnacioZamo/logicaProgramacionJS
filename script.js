5d99d4b8fb0b2e56c4ec80137f240aacd9db107a
Ignacio Zamo <ignaciozamo1@gmail.com>
2024-04-28 22:03:17 -0300

    Se suben primeros ejercicios de js

---
+++ script.js
@@ -0,0 +1,44 @@
+// ejercicio1
+const ej1 = document.getElementById("ej1");
+let ejArray = []; 
+ej1.addEventListener("click",()=>{
+    for (let i = 1; i<=10; i++){
+        if(i!== 5){
+            let multEj1 = i*5;
+            ejArray.push(multEj1);
+        }
+    }
+
+    let mensaje = `La tabla de multiplicar del 5, excluyendo el 5, es: ${ejArray.join(", ")}`
+
+    alert(mensaje)
+
+})
+
+
+// ejercicio 2
+const ej2 = document.getElementById("ej2");
+
+ej2.addEventListener("click",()=>{
+    let n1 = parseInt(prompt("Ingrese numero 1"));
+    let n2 = parseInt(prompt("Ingrese numero 2"));
+
+    if (typeof n1 === 'number' && typeof n2 === 'number' ){
+        if(n1 > n2){
+            alert("El numero: " + n1 + " es el numero mayor");
+        } else if (n1<n2){
+            alert (`El numero: ${n2} es el numero mayor`);
+        } else if (n1 == n2){
+            alert (`Ambos numeros son iguales`)
+        }
+    }
+    else{
+        alert ("Valor o valores ingresados incorrectos")
+    }
+})
+
+//ejercicio 3
+
+const ej3 = document.addEventListener("ej3");
+
+
