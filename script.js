1e47c48ea489e7c638c793775363ecf63c2103f5
Ignacio Zamo <ignaciozamo1@gmail.com>
2024-06-08 21:44:13 -0300

    Se agrega cuarto ejercicio: arrays y suma de elementos dentro - se practica el uso de for, if, push de array

--- script.js
+++ script.js
@@ -51,3 +51,36 @@ numEj3.addEventListener("click",()=>{
     }
 
 })
+
+
+//ejercicio 4
+
+const numEj4 = document.getElementById("ej4")
+const cantNumEj4 = [];
+
+numEj4.addEventListener("click",()=>{
+    let cantNumEj4Ingresado = parseInt(prompt("Ingrese cantidad de numeros (maximo 10) que querrá ingresar: "))
+    if (cantNumEj4Ingresado <= 10) {        
+        for(let i = 0; i<=(cantNumEj4Ingresado - 1); i++){
+            let numEj4Eleg = parseInt(prompt("Ingrese un numero: "))
+            cantNumEj4.push(numEj4Eleg);
+        }
+        let eleccionEj4 = parseInt(prompt("Elija si quiere (1)-Sumar o (2)-Multiplicar"))
+        if(eleccionEj4 == 1 || eleccionEj4 == 2){
+            if (eleccionEj4 == 1){
+                let suma = 0;
+                for (let i = 0; i<cantNumEj4.length; i ++ ){
+                    suma += cantNumEj4[i];
+                }
+                console.log(suma)
+            } else {
+                alert("usted eligio una suma")
+            }
+       } else {
+        alert("Ha elegido una opcion invalida")
+       }
+    } else {
+        alert("Ha ingresado un valor incorrecto o mayo a 10")
+    }
+})
+
